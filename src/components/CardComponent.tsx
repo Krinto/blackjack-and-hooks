78c2149e6464881eaa10aa6e70af049098232913
Kieran Matherson <krinto@hotmail.com>
2019-06-27 00:35:07 +1200

    :bug: don't render facedown card until dealer turn

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -5,7 +5,9 @@ export const CardComponent: React.FC<React.PropsWithChildren<{card: Card}>> = ({
     return (
         <div className="card-wrapper"> 
             <div className={"card" + (card.hidden ? " hidden" : "")}>
-                <img  className="front" alt={`${card.value}_of_${card.suit}`} src={require(`../assets/images/${card.value}_of_${card.suit}.svg`)} />
+                {card.hidden ? 
+                (<img className="front"/>): 
+                (<img className="front" alt={`${card.value}_of_${card.suit}`} src={require(`../assets/images/${card.value}_of_${card.suit}.svg`)} />)}                
                 <img className="back" alt="card back" src={require('../assets/images/reverse_card.svg')} />
             </div>
         </div>
